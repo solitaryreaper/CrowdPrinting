9fee8f4c8535a4f43a383bcc7acc67c4ebc425cd
Prakhar Panwaria <prakhar.panwaria9@gmail.com>
2014-03-01 04:53:28 -0600

    Added create accoutn page.

--- app/controllers/JoinController.java
+++ app/controllers/JoinController.java
@@ -2,11 +2,12 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.join_form;
 
 public class JoinController extends Controller
 {
 	public static Result createID()
 	{
-		return ok("Inside Join page");
+		return ok(join_form.render());//"Inside Join page");
 	}
 }
